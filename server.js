b3e00637426ca746c73ceb7a39dafdecc1ba3390
skyprotect <skyprotect@gmail.com>
2026-07-17 17:57:55 +0700

    Auto release version v10.98

--- server.js
+++ server.js
@@ -3672,7 +3672,7 @@ app.post('/api/exit-kiosk', authenticateAdminToken, (req, res) => {
 const https = require('https');
 const { spawn } = require('child_process');
 
-const APP_VERSION = '10.101';
+const APP_VERSION = '10.98';
 
 // 2. API lấy danh sách từ vựng tự nạp
 app.get('/api/custom-vocabulary', (req, res) => {
