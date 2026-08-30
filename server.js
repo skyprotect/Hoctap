6e57376e391fb841f8402097bfaeae852200e35f
skyprotect <skyprotect@gmail.com>
2026-08-30 07:44:25 +0700

    Auto release version v13.24

--- server.js
+++ server.js
@@ -4528,7 +4528,7 @@ app.post('/api/exit-kiosk', authenticateAdminToken, (req, res) => {
 const https = require('https');
 const { spawn } = require('child_process');
 
-const APP_VERSION = '13.23';
+const APP_VERSION = '13.24';
 
 
 // 2. API lấy danh sách từ vựng tự nạp
